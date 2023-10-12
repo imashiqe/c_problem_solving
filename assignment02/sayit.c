f223a6ef0a50a625629d21f217775e1199c575ba
Ashiqur Rahman <imashiqe@gmail.com>
2023-10-13 00:18:53 +0600

    I Want More Assignments problem  solved

---
+++ assignment02/sayit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    for (int i = 1; i <= N; i++) {
+        printf("%d.  I Want More Assignments \n", i);
+    }
+
+    return 0;
+}
