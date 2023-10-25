db5f0103b649dbc35c767a28d828573eab1ea894
Ashiqur Rahman <imashiqe@gmail.com>
2023-10-25 22:04:54 +0600

    simple pattern

---
+++ module13/p_prac01.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+   
+{
+    int n,k=1;
+   scanf("%d",&n);
+    for (int  i= 1; i<=n; i++)
+    {
+        for (int  j=1; j<=k; j++)
+        {
+           printf("*");
+        }
+        k++;
+       printf("\n");
+    }
+    
+    
+    return 0;
+}
